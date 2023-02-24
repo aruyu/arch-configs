df1bfa88381a4bf6d09eacd7ae3cb84d79fb8917
Asta <vine9151@gmail.com>
2023-02-24 14:33:30 +0900

    refactor: functional change to make 'echo' colorful
    230224_14_35

--- install_system.sh
+++ install_system.sh
@@ -10,9 +10,33 @@
 
 
 
+T_CO_RED='\e[1;31m'
+T_CO_YELLOW='\e[1;33m'
+T_CO_GREEN='\e[1;32m'
+T_CO_BLUE='\e[1;34m'
+T_CO_GRAY='\e[1;30m'
+T_CO_NC='\e[0m'
+
+CURRENT_PROGRESS=0
+
+function script_print()
+{
+  echo -ne "$T_CO_BLUE[SCRIPT]$T_CO_NC$1"
+}
+
+function script_print_notify()
+{
+  echo -ne "$T_CO_BLUE[SCRIPT]$T_CO_NC$T_CO_GREEN-Notify- $1$T_CO_NC"
+}
+
+function script_print_error()
+{
+  echo -ne "$T_CO_BLUE[SCRIPT]$T_CO_NC$T_CO_RED-Error- $1$T_CO_NC"
+}
+
 function error_exit()
 {
-  echo -ne "Error: $1\n"
+  script_print_error "$1\n\n"
   exit 1
 }
 
@@ -121,6 +145,11 @@ REALEND
 #   Starting codes in blew
 #/
 
+if [[ $EUID -ne 0 ]]; then
+  error_exit "This script must be run as ROOT!"
+fi
+
+
 rmmod pcspkr
 rmmod snd_pcsp
 ping -c 3 archlinux.org || error_exit "Internet connection failed."
@@ -135,8 +164,8 @@ genfstab -U /mnt >> /mnt/etc/fstab
 config_arch || error_exit "Arch configuration failed."
 fdisk -l
 cat /mnt/etc/fstab
-echo "Default passwords is all the same as ID"
-echo "Please change the passwords."
+script_print_notify "Default passwords is all the same as ID.\n"
+script_print_notify "Please change the passwords.\n"
 
 umount -R /mnt
-echo "All successfully done."
+script_print_notify "All successfully done.\n"
