217d379fbda8fd58f97adc7ca72287c3ee984e85
Aru <vine9151@gmail.com>
2024-03-21 16:05:06 +0900

    feat: add new package for event test; 'evtest'
    240321_16_05

--- tools/install_swayfx.sh
+++ tools/install_swayfx.sh
@@ -95,7 +95,7 @@ function install_user_apps()
   sudo pacman -S --needed --noconfirm chromium firefox foot
   sudo pacman -S --needed --noconfirm libreoffice-still gimp inkscape xournalpp
   sudo pacman -S --needed --noconfirm mpv mpc ncmpcpp viewnior copyq grim slurp
-  sudo pacman -S --needed --noconfirm htop neofetch gsimplecal wev
+  sudo pacman -S --needed --noconfirm htop neofetch gsimplecal wev evtest
 
   sudo pacman -S --needed --noconfirm docker minicom remmina freerdp
   sudo systemctl enable docker.service
