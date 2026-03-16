3c088202094d56e19d625eb310f719f1f3f17b96
Aru <vine9151@gmail.com>
2026-03-16 10:48:06 +0900

    chore: change regedit text to batch excutable
    260316_10_48

---
+++ configs/regedit.bat
@@ -0,0 +1,13 @@
+@echo off
+REM=
+REM  NOTE      - regedit.bat
+REM  Author    - Aru
+REM
+REM  Created   - 2026.03.15
+REM  Github    - https://github.com/aruyu
+REM  Contact   - vine9151@gmail.com
+REM
+
+
+
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\TimeZoneInformation" /v RealTimeIsUniversal /t REG_DWORD /d 1
